339d112c16aa54c3cb8dff1e4c8bbd8d24073c39
arslan759 <arslan.obaid759@gmail.com>
2022-11-26 04:26:42 +0500

    added project files

---
+++ src/birthday-reminder/BirthdayReminder.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { Persons } from './data'
+import './birthday.css'
+
+function BirthdayReminder() {
+    const [persons , setPersons] = useState(Persons)
+
+    const removeItem = (id) => {
+        let newPersons = persons.filter((item)=> item.id !== id)
+        setPersons(newPersons)
+    }
+  return (
+    <React.Fragment>
+        <div className='reminder container'>
+            <h2>{persons.length} birthdays total</h2>
+            {
+                persons.map((item)=>(
+                    <div className='row d-flex mt-3'>
+                        <div className='col-2 image me-2'>
+                            <img src={item.image} alt="" />
+                        </div>
+                        <div className='col'>
+                            <h4>{item.name}</h4>
+                            <p>{item.age} years</p>
+                        </div>
+                        <div className='col-3'>
+                            <button type='button' className='remove btn mt-3 w-100' onClick={()=>removeItem(item.id)}>Remove</button>
+                        </div>
+                    </div>
+                ))
+            }
+            <button type='button' className='remove-all btn mt-3 w-100' onClick={()=>setPersons([])}>Clear All</button>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default BirthdayReminder
---
+++ src/birthday-reminder/data.js
@@ -0,0 +1,37 @@
+export const Persons = [
+  {
+    id: 1,
+    name: "Peter Parker",
+    age: 22,
+    image:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 2,
+    name: "Susan Jane",
+    age: 25,
+    image:
+      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 3,
+    name: "Alex Charles",
+    age: 23,
+    image:
+      "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 4,
+    name: "Emma Stone",
+    age: 18,
+    image:
+      "https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 5,
+    name: "Shake And Bake",
+    age: 21,
+    image:
+      "https://images.pexels.com/photos/2103962/pexels-photo-2103962.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+];
